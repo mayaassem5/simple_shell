849c4a23ce6f9ce00e2cd5906d7442c1c9aaf61c
mayaassem5 <mayaassem5@gmail.com>
2023-11-13 09:35:50 -0800

    editing betty done

--- simple_shell.c
+++ simple_shell.c
@@ -4,7 +4,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	pid_t child;
+{
+	pid_t child;
 	char *line = NULL, **command = NULL, *path;
 	size_t l = 0;
 	int status = 0, r = 0;
@@ -19,34 +20,22 @@ int main(void)
 			continue;
 		command = split(line);
 		CHECK_NULL(command);
-		if (built(line, command, &r) == 0)
+		child = fork();
+		if ((built(line, command, &r) == 0) && child == 0)
 		{
-			child = fork();
-			if (child == 0)
-			{
-				path = findpath(command[0], &r);
-				if (path == NULL)
-				{
-					freep(line, command);
-					exit(EXIT_FAILURE);
-				}
-				if (execve(path, command, environ) == -1)
-				{
-					freep(line, command);
-					exit(r);
-				}
-			}
-			else
-			{
-				wait(&status);
-				freep(line, command);
-				if (WIFEXITED(status))
-					r = WEXITSTATUS(status);
-			}
-			line = NULL;
+			path = findpath(command[0], &r);
+			if (path == NULL && (freep(line, command), 1))
+				exit(EXIT_FAILURE);
+			if (execve(path, command, environ) == -1 && (freep(line, command), 1))
+				exit(r);
 		}
-		else
-			freepointer(command);
+		else if (freepointer(command), 1)
+			continue;
+		wait(&status);
+		freep(line, command);
+		if (WIFEXITED(status))
+			r = WEXITSTATUS(status);
+		line = NULL;
 	}
 	free(line);
 	exit(status);
