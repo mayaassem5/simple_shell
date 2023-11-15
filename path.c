83b020f27ae152f008979741c0121ca375687db1
mayaassem5 <mayaassem5@gmail.com>
2023-11-15 13:46:45 -0800

    edit path

--- path.c
+++ path.c
@@ -7,29 +7,35 @@
  */
 char *findpath(char *command, int *r)
 {
-	char *path, *toprint;
+	char *path, *toprint, *path_copy, *tok;
 	struct stat stats;
-	char *current, *tok;
+	char *current;
+
+	if (command == NULL)
+		return (NULL);
 
 	if (stat(command, &stats) == 0)
 		return (command);
 
 	path = _getenv("PATH");
-	tok = strtok(path, ":");
 	toprint = command;
 	command = _concat("/", command);
+
+	path_copy = _strdup(path);
+	tok = strtok(path_copy, ":");
 
 	while (tok != NULL)
 	{
 		current = _concat(tok, command);
 		if (stat(current, &stats) == 0)
 		{
+			free(path_copy);
 			return (current);
 		}
 		free(current);
 		tok = strtok(NULL, ":");
 	}
-
+	free(path_copy);
 	error(path, len(command), toprint);
 	printstr(": not found", 0);
 	free(command);
